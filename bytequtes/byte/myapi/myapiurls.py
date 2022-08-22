020348b86516dba82f1425a767811e4b14c309ad
MAYANK <94459776+MAYANKMAYANK@users.noreply.github.com>
2022-08-22 08:37:07 +0530

    Update myapiurls.py

--- bytequtes/byte/myapi/myapiurls.py
+++ bytequtes/byte/myapi/myapiurls.py
@@ -2,9 +2,18 @@ from django.conf.urls import url
 from . import views
 # from .views import add_data
 
+#url code
+from myapi.views import Curd
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'api', Curd, basename='user')
+
+
 urlpatterns=[
     url(r'^all_data',views.all_data,name='all_data'),
     url(r'^add_data',views.add_data,name='add_data'),
     url(r'^delete_data/(?P<uid>\d+)/$',views.delete_data,name='delete_data'),
     url(r'^upd_data/(?P<uid>\d+)/$',views.upd_data,name='upd_data')
 ]
+
+urlpatterns += router.urls
